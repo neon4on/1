74377be31dbcaa1dd5f9240fe8ffeafea8dae11f
Valeriy Kamenskikh <86181396+neon4on@users.noreply.github.com>
2023-03-03 12:52:28 +0400

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+class List
+{
+public:
+	List();
+	~List();
+
+	void push_back(T data);
+	int GetSize(){return Size;}
+
+private:
+	template <typename T>
+	class Node 
+	{
+	public:
+		Node* pNext;
+		T data;
+
+		Node(T data = T(), Node* pNext = nullptr)
+		{
+			this->data = data;
+			this->pNext = pNext;
+		}
+	};
+
+	int Size;
+	Node<T> *head;
+};
+
+template <typename T>
+List<T>::List()
+{
+	Size = 0;
+	head = nullptr;
+}
+
+template <typename T>
+List<T>::~List()
+{
+
+}
+
+template <typename T>
+void List<T>::push_back(T data)
+{
+	if (head == nullptr)
+	{
+		head = new Node<T>(data);
+	}
+	else
+	{
+		Node<T>* current = this->head;
+		while (current -> pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node<T>(data);
+	}
+
+	Size++;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	List<int> lst;
+	lst.push_back(5);
+	lst.push_back(10);
+	lst.push_back(22);
+
+	cout << lst.GetSize() << endl;
+
+	return 0;
+}
